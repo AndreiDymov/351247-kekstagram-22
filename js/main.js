c388d1fdfb995f8e05e818ef436b372663c2dda2
Andrei <dmvandrey@gmail.com>
2021-02-02 00:16:05 +0300

    utility functions

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Функция, возвращающая случайное целое число из переданного диапазона включительно. 
+// sourse: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+// Функция для проверки максимальной длины строки.
+
+function allowedLineLength(str,maxlength){
+    if(maxlength >= str.length){
+        return true;
+    } else {
+      return false;
+      };
+}
