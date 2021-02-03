420c9f42f4dabb5b6dea2bb2a7d56fb040e4dc18
Andrei <dmvandrey@gmail.com>
2021-02-03 23:25:13 +0300

    need more functions

--- js/main.js
+++ js/main.js
@@ -3,18 +3,31 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно. 
 // sourse: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-(function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-});
+function getRandomIntInclusive(a,b) {
+  let min;
+  let max;
+  if (a > 0 && b > 0) {  // Проверка на отрицательность диапазона
+    if (a < b) {
+      a = min;
+      b = max; 
+    } else {
+      a = max;
+      b = min;
+    }
+    min = Math.ceil(min);
+    max = Math.floor(max);  
+    return Math.floor(Math.random() * (max - min + 1)) +min;  
+  }
+}
+
+
+getRandomIntInclusive ();
 
 // Функция для проверки максимальной длины строки.
 
-(function allowedLineLength(str,maxlength){
-  if(maxlength >= str.length){
-    return true;
-  } else {
-    return false;
-  }
-})
+let maxLength;
+let str;
+
+let checkMaxLengthStr = (maxLength >= str.length) ? true : false;
+
+checkMaxLengthStr ();
